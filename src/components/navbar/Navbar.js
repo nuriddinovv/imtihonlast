28a8ed23e47b4971cc9d5d18c006145e0ee71b1b
nuriddinovv <blackscarp1onoff@gmail.com>
2024-07-06 19:14:56 +0500

    modal

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { HiMiniBars3BottomLeft } from "react-icons/hi2";
+import { FiSearch } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
+import axios from "axios";
 
 export default function Navbar() {
   const { t, i18n } = useTranslation();
-  console.log(i18n);
+  const [openMenuModal, setOpenMenuModal] = useState(false);
+  const [modalData, setModalData] = useState();
+
   const toggleLanguage = () => {
     const newLang = i18n.language === "uz" ? "ru" : "uz";
     i18n.changeLanguage(newLang);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(
+          "https://admin.ht-med.uz/api/v1/category-list/"
+        );
+        setModalData(result.data.results);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="nav">
       <div className="nav_container mx-auto">
-        <div className="nav_container_content max-w-[1200px] mx-auto flex text-white justify-between items-center">
+        <div className="nav_container_content max-w-[80%] mx-auto flex text-white justify-between items-center">
           <div className="nav_container_content_logo w-[270px] h-[145px] overflow-hidden flex justify-center items-center">
             <Link to={"/"}>
               <img
@@ -91,6 +112,76 @@ export default function Navbar() {
           </div>
         </div>
       </div>
+      <div className="nav_search max-w-[80%] mx-auto flex items-center gap-4">
+        <div className="nav_search_catalog">
+          <button
+            className="font-[500]"
+            onClick={() => {
+              setOpenMenuModal(true);
+            }}
+          >
+            <HiMiniBars3BottomLeft />
+            {t("home.catalog")}
+          </button>
+        </div>
+        <div className="nav_search_input w-full ">
+          <form
+            className="flex "
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input
+              className="w-full"
+              placeholder={t("home.search")}
+              type="text"
+            />
+            <button>
+              <FiSearch />
+            </button>
+          </form>
+        </div>
+      </div>
+
+      {openMenuModal && (
+        <div
+          onClick={() => {
+            setOpenMenuModal(false);
+          }}
+          className="menuModal absolute top-0 w-[100vw] h-[100vh] flex items-center justify-center z-50"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="menuModalContent w-[80%] mx-auto bg-white rounded-lg px-[20px] py-[24px] z-50"
+          >
+            <div className="menuModalContentHeader text-end ">
+              <span
+                className="cursor-pointer flex justify-end mb-3 text-[24px]"
+                onClick={() => {
+                  setOpenMenuModal(false);
+                }}
+              >
+                <IoMdClose />
+              </span>
+            </div>
+            <div className="menuModalContentRow gap-4">
+              {modalData?.map((item) => (
+                <div
+                  key={item.id}
+                  className="card1 p-[20px] flex items-center justify-center gap-4 cursor-pointer rounded-[20px] hover:shadow-lg transition-all duration-500"
+                >
+                  <div className="w-[80px] h-[80px] overflow-hidden">
+                    <img src={item.image} className="w-full" alt="" />
+                  </div>
+                  <p className="flex text-center items-center font-[600]">
+                    {item.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/homePage/Home.js
+++ src/pages/homePage/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='container w-[80%] mx-auto'>
+      asdasd
+    </div>
   )
 }
