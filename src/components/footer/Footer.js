85e64865b414b88424727785338fa8aec0f01f47
nuriddinovv <blackscarp1onoff@gmail.com>
2024-07-11 18:09:50 +0500

    a

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,117 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import logo from "../../assets/navbar/logo.webp";
+import i18n from "../../i18n";
+import { BiSupport } from "react-icons/bi";
+import facebook from "../../assets/footer/facebook.png";
+import telegram from "../../assets/footer/telegram.png";
+import instagram from "../../assets/footer/instagram.png";
+import youtube from "../../assets/footer/youtube.png";
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="">
+      <div className="footer py-[63px] text-white">
+        <div className=" w-[95%] md:w-[80%] mx-auto block md:grid grid-cols-3 ">
+          <div
+            className="flex flex-col items-center "
+            style={{ borderBottom: "1px dotted white" }}
+          >
+            <img className="w-[200px] md:w-[270px]" src={logo} alt="" />
+            <p className=" font-semibold text-center md:text-start mb-2">
+              {i18n.t("footer.title")}
+            </p>
+            <div className=" gap-3 hidden  sm:flex p-5 bg-white/10 rounded-2xl">
+              <span className="text-green-500 text-[40px]">
+                <BiSupport />
+              </span>
+              <div className="">
+                <p className="text-[14px]">{i18n.t("footer.support")}</p>
+                <Link className="text-[20px]" to={"tel:999999999"}>
+                  +998 (99) 311 25 52
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div
+            className="flex flex-col  mt-8"
+            style={{ borderBottom: "1px dotted white" }}
+          >
+            <h1 className="text-[20px] ">{i18n.t("footer.menu")}</h1>
+            <div className="grid grid-rows-5 grid-cols-2 gap-[15px] mt-3">
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.mainpage")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.catalog")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.about")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.news")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.blog")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.contacts")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.company")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.winning")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.team")}
+                </Link>
+              </span>
+              <span>
+                <Link className="" style={{ borderBottom: "1px dotted green" }}>
+                  {i18n.t("footer.galery")}
+                </Link>
+              </span>
+            </div>
+          </div>
+          <div
+            className="flex flex-col mt-8"
+            style={{ borderBottom: "1px dotted white" }}
+          >
+            <h1 className="text-[20px] ">{i18n.t("footer.newstext")}</h1>
+            <p>{i18n.t("footer.newsp")}</p>
+            <div className="icons relative h-[200px] w-[200px] mx-auto">
+              <Link to={"#"} className="absolute left-[55px]">
+                <img className="w-[88px]" src={facebook} alt="" />
+              </Link>
+              <Link to={"#"} className="absolute top-[45px]">
+                <img className="w-[88px]" src={telegram} alt="" />
+              </Link>
+              <Link to={"#"} className="absolute top-[45px] left-[110px]">
+                <img className="w-[88px]" src={youtube} alt="" />
+              </Link>
+              <Link to={"#"} className="absolute top-[45%] left-[55px]">
+                <img className="w-[88px]" src={instagram} alt="" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/homePage/Home.js
+++ src/pages/homePage/Home.js
@@ -6,12 +6,22 @@ import { Resquet } from "../../components/axios/Axios"; // Check if this import 
 import { Link, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Loader from "../../components/loader/Loader";
-import { FaCircleArrowRight } from "react-icons/fa6";
+import { FaCircleArrowRight, FaLocationDot } from "react-icons/fa6";
 import tobottom from "../../assets/home/tobottom.png";
 import totop from "../../assets/home/totop.png";
 import toright from "../../assets/home/toright.png";
 import end from "../../assets/home/endt.png";
-import { Skeleton } from "@mui/material";
+import { IoMailOutline } from "react-icons/io5";
+import { IoIosPhonePortrait } from "react-icons/io";
+import telegramicon from "../../assets/home/talegramicon.webp";
+import whatsappicon from "../../assets/home/whatsappicon.webp";
+import skypeicon from "../../assets/home/skype.webp";
+import wechaticon from "../../assets/home/wechat.webp";
+import youtubeicon from "../../assets/home/youtube.webp";
+import facebookicon from "../../assets/home/facebook.webp";
+import instaicon from "../../assets/home/instagram.webp";
+import { TextField } from "@mui/material";
+
 export default function Home() {
   const navigate = useNavigate();
   const { i18n } = useTranslation();
@@ -396,10 +406,158 @@ export default function Home() {
             <h3 className="w-auto mr-4 my-6">{i18n.t("home.contact")}</h3>
             <span className="bg-[#292929] h-[2px] flex-grow"></span>
           </div>
-          <div className="contact_about w-[631px]"></div>
-          <div className="contact_message"></div>
+          <div className="md:flex justify-between gap-7 gap-y-3.5">
+            <div className="contact_about flex flex-col sm:flex-row justify-between gap-7 w-full md:w-[631px] p-[31px] bg-gray-200/60 rounded-3xl">
+              <div className="flex flex-col ">
+                <div className="address flex gap-4">
+                  <div className="icon">
+                    <FaLocationDot />
+                  </div>
+                  <div className="text justify-between mb-3">
+                    <h4 className="text-gray-400 mb-2">
+                      {i18n.t("home.address")}
+                    </h4>
+                    <p className="text-[14px]">{i18n.t("home.address1")}</p>
+                  </div>
+                </div>
+                <div className="mail flex gap-4">
+                  <div className="icon">
+                    <IoMailOutline />
+                  </div>
+                  <div className="text justify-between mb-3">
+                    <h4 className="text-gray-400 mb-2">
+                      {i18n.t("home.mail")}
+                    </h4>
+                    <p className="text-[14px]">
+                      <Link to={"mailto:hi-tech_orient_medical@mail.ru"}>
+                        hi-tech_orient_medical@mail.ru
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+                <div className="address flex gap-4">
+                  <div className="icon">
+                    <IoIosPhonePortrait />
+                  </div>
+                  <div className="text justify-between mb-3">
+                    <h4 className="text-gray-400 mb-2">
+                      {i18n.t("home.phoneNum")}
+                    </h4>
+                    <Link className="font-bold" to={"tel:998971210789"}>
+                      +998 (97) 131 07 89
+                    </Link>{" "}
+                    <br />
+                    <Link className="font-bold" to={"tel:998971210789"}>
+                      +998 (97) 131 07 89
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col justify-between w-[473px]">
+                <div className="">
+                  <h4 className="text-[18px] font-bold mb-3">
+                    {i18n.t("home.contactus")}
+                  </h4>
+                  <div className="icons grid grid-cols-4 gap-4 ">
+                    <Link to={"#"}>
+                      <img
+                        src={telegramicon}
+                        alt="telegram"
+                        className="w-[50px] sm:w-full"
+                      />
+                    </Link>
+                    <Link to={"#"}>
+                      <img
+                        src={whatsappicon}
+                        alt="telegram"
+                        className="w-[50px] sm:w-full"
+                      />
+                    </Link>
+                    <Link to={"#"}>
+                      <img
+                        src={skypeicon}
+                        alt="telegram"
+                        className="w-[50px] sm:w-full"
+                      />
+                    </Link>
+                    <Link to={"#"}>
+                      <img
+                        src={wechaticon}
+                        alt="telegram"
+                        className="w-[50px] sm:w-full"
+                      />
+                    </Link>
+                  </div>
+                </div>
+                <div className="">
+                  <h4 className="text-[18px] font-bold mb-3">
+                    {i18n.t("home.socials")}
+                  </h4>
+                  <div className="icons grid grid-cols-4 gap-4 ">
+                    <Link to={"#"}>
+                      <img
+                        src={youtubeicon}
+                        alt="telegram"
+                        className="w-[50px] sm:w-full"
+                      />
+                    </Link>
+                    <Link to={"#"}>
+                      <img
+                        src={whatsappicon}
+                        alt="telegram"
+                        className="w-[50px] sm:w-full"
+                      />
+                    </Link>
+                    <Link to={"#"}>
+                      <img
+                        src={facebookicon}
+                        alt="telegram"
+                        className="w-[50px] sm:w-full"
+                      />
+                    </Link>
+                    <Link to={"#"}>
+                      <img
+                        src={instaicon}
+                        alt="telegram"
+                        className="w-[50px] sm:w-full"
+                      />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="contact_message w-full sm:w-[519px] px-[42px] py-[33px] bg-gray-200/60  rounded-3xl">
+              <h2 className="text-2xl font-bold">
+                {i18n.t("home.application")}
+              </h2>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                <TextField
+                  className="w-full p-2"
+                  label={i18n.t("home.name")}
+                  variant="standard"
+                />
+                <TextField
+                  className="w-full p-2"
+                  label={i18n.t("home.num")}
+                  variant="standard"
+                />
+                <TextField
+                  className="w-full p-2"
+                  label={i18n.t("home.text")}
+                  variant="standard"
+                />
+                <button className="px-[30px] py-[10px] rounded-2xl bg-[#298dc6] text-white mt-5">
+                  {i18n.t("home.sendbtn")}
+                </button>
+              </form>
+            </div>
+          </div>
           <iframe
-            className="rounded-[25px]"
+            className="rounded-[25px] mt-8"
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d749.9194707851482!2d69.255519!3d41.250574!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b278f47926f%3A0x58c5ded3abff2147!2sHi-tech%20Orient%20Medical!5e0!3m2!1sen!2sus!4v1720526472472!5m2!1sen!2sus"
             width="100%"
             height="450"
